d2f36a677419acd92d81722ed6550a0ee77a8f7c
Piya Miang-Lae <piyaboy097@gmail.com>
2025-08-03 02:29:28 +0700

    Update tab bar styling and configuration

--- mobile/app/(root)/_layout.jsx
+++ mobile/app/(root)/_layout.jsx
@@ -1,35 +1,11 @@
 
 import { useUser } from "@clerk/clerk-expo";
-import { Redirect, Tabs, useRouter } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import { Redirect, Tabs } from "expo-router";
+import { CustomTabBar } from "./_tabBar";
 import { COLORS } from "../../constants/colors";
-import { View, TouchableOpacity } from "react-native";
 
-const AddButton = ({ children, onPress }) => (
-  <TouchableOpacity
-    style={{
-      top: -25,
-      justifyContent: "center",
-      alignItems: "center",
-      width: 60,
-      height: 60,
-      borderRadius: 30,
-      backgroundColor: COLORS.primary,
-      shadowColor: COLORS.shadow,
-      shadowOffset: { width: 0, height: 4 },
-      shadowOpacity: 0.3,
-      shadowRadius: 4,
-      elevation: 5,
-    }}
-    onPress={onPress}
-  >
-    {children}
-  </TouchableOpacity>
-);
-
-export default function TabLayout() {
+export default function Layout() {
   const { isSignedIn, isLoaded } = useUser();
-  const router = useRouter();
 
   if (!isLoaded) return null; // this is for a better ux
 
@@ -42,50 +18,12 @@ export default function TabLayout() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: COLORS.primary,
         tabBarInactiveTintColor: COLORS.textLight,
-        tabBarStyle: {
-          position: "absolute",
-          bottom: 20,
-          left: 20,
-          right: 20,
-          elevation: 0,
-          backgroundColor: COLORS.white,
-          borderRadius: 15,
-          height: 60,
-          borderTopWidth: 0,
-          shadowColor: COLORS.shadow,
-          shadowOffset: { width: 0, height: -2 },
-          shadowOpacity: 0.1,
-          shadowRadius: 3,
-        },
       }}
+      tabBar={props => <CustomTabBar {...props} />}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="reports"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="bar-chart-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add" size={size * 1.5} color={COLORS.white} />
-          ),
-          tabBarButton: (props) => (
-            <AddButton {...props} onPress={() => router.push("/create")} />
-          ),
-        }}
-      />
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="reports" />
+      <Tabs.Screen name="create" />
     </Tabs>
   );
 }
